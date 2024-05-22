0be2ff0c55f409db21c8a2dddb274a1ecbf058be
Vsevolod Fadeev <sevafad@gmail.com>
2024-05-22 21:04:49 +0300

    Install ejs.

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 const express = require("express")
 
+const path = require("path")
+
 const app = express()
 
+app.set("views", path.join(__dirname, "/views"))
+
+app.set("view engine", "ejs")
+
 app.get("/", function (req, res) {
-  res.send("Hello world")
+  res.render("index", { title: "Hello world" })
 })
 
 app.listen(3000)
