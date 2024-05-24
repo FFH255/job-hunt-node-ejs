815759171a5919a0ce586f5fa507174ff529b9ca
Vsevolod Fadeev <sevafad@gmail.com>
2024-05-24 22:56:56 +0300

    Create public folder with static files and serve it.

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ const app = express()
 
 app.set("views", path.join(__dirname, "/views"))
 
+app.use(express.static("src/public"))
+
 app.set("view engine", "ejs")
 
 app.get("/", function (req, res) {
